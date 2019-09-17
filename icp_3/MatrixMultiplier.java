aa875b19e316b8959bffbc72bc16245fb40c409b
mikelhomeless <mthomer1027@gmail.com>
2019-09-16 22:43:23 -0500

    yeet

--- icp_3/MatrixMultiplier.java
+++ icp_3/MatrixMultiplier.java
@@ -16,34 +16,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class MatrixMultiplier {
 
     public static class Map
-            extends org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, Text> {
-        @Override
+            extends Mapper<LongWritable, Text, Text, Text> {
+
         public void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
             Configuration conf = context.getConfiguration();
             int m = Integer.parseInt(conf.get("m"));
             int p = Integer.parseInt(conf.get("p"));
             String line = value.toString();
-            // (M, i, j, Mij);
+
             String[] tokens = line.split(",");
-            Text outputKey = new Text();
-            Text outputValue = new Text();
+            Text outKey = new Text();
+            Text outValue = new Text();
             if (tokens[0].equals("M")) {
                 for (int k = 0; k < p; k++) {
-                    outputKey.set(tokens[1] + "," + k);
-                    // outputKey.set(i,k);
-                    outputValue.set(tokens[0] + "," + tokens[2]
+                    outKey.set(tokens[1] + "," + k);
+
+                    outValue.set(tokens[0] + "," + tokens[2]
                             + "," + tokens[3]);
-                    // outputValue.set(M,j,Mij);
-                    context.write(outputKey, outputValue);
+
+                    context.write(outKey, outValue);
                 }
             } else {
-                // (N, j, k, Njk);
+
                 for (int i = 0; i < m; i++) {
-                    outputKey.set(i + "," + tokens[2]);
-                    outputValue.set("N," + tokens[1] + ","
+                    outKey.set(i + "," + tokens[2]);
+                    outValue.set("N," + tokens[1] + ","
                             + tokens[3]);
-                    context.write(outputKey, outputValue);
+                    context.write(outKey, outValue);
                 }
             }
         }
@@ -55,16 +55,16 @@ public class MatrixMultiplier {
         public void reduce(Text key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
             String[] value;
-            //key=(i,k),
-            //Values = [(M/N,j,V/W),..]
-            HashMap<Integer, Float> hashA = new HashMap<Integer, Float>();
-            HashMap<Integer, Float> hashB = new HashMap<Integer, Float>();
+
+            // using a hashmap here because it's easier than sorting on a secondary key within a java vector
+            HashMap<Integer, Float> matrixM = new HashMap<Integer, Float>();
+            HashMap<Integer, Float> matrixN = new HashMap<Integer, Float>();
             for (Text val : values) {
                 value = val.toString().split(",");
-                if (value[0].equals("M")) {
-                    hashA.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+                if (value[0] == "M") {
+                    matrixM.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
                 } else {
-                    hashB.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+                    matrixN.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
                 }
             }
             int n = Integer.parseInt(context.getConfiguration().get("n"));
@@ -72,8 +72,8 @@ public class MatrixMultiplier {
             float m_ij;
             float n_jk;
             for (int j = 0; j < n; j++) {
-                m_ij = hashA.containsKey(j) ? hashA.get(j) : 0.0f;
-                n_jk = hashB.containsKey(j) ? hashB.get(j) : 0.0f;
+                m_ij = matrixM.containsKey(j) ? matrixM.get(j) : 0.0f;
+                n_jk = matrixN.containsKey(j) ? matrixN.get(j) : 0.0f;
                 result += m_ij * n_jk;
             }
             if (result != 0.0f) {
